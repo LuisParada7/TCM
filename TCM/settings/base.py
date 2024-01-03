4ab5369b3e1027148136cb52768e4fde1200da63
LuisParada7 <146735108+LuisParada7@users.noreply.github.com>
2024-01-03 10:53:18 -0500

    basee

--- TCM/settings/base.py
+++ TCM/settings/base.py
@@ -258,8 +258,12 @@ WSGI_APPLICATION = 'TCM.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = db.SQLITE
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
